6be2783781182038299fefc38e5aa5ef60efef0a
dxr2587698334 <2587698334@qq.com>
2019-05-05 19:57:40 +0800

    Create IPr.java

---
+++ src/com/yanzi/service/IPr.java
@@ -0,0 +1,17 @@
+package com.yanzi.service;
+
+import com.yanzi.bean.Pr;
+
+public interface IPr {
+
+	public void addPr(Pr pr);
+
+	public void updatePr(Pr pr);
+
+	// 删除原有的关系
+	public void delPr(int r_id);
+	
+	// 批量删除
+	public int delBatchPr(int r_id);
+
+}
