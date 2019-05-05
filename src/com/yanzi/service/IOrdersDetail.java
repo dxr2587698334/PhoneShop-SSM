fe322318b0a43c3c0503cb75d97554393fa05177
dxr2587698334 <2587698334@qq.com>
2019-05-05 19:25:24 +0800

    Update IOrdersDetail.java

--- src/com/yanzi/service/IOrdersDetail.java
+++ src/com/yanzi/service/IOrdersDetail.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
-import com.openlab.bean.OdCustom;
-import com.openlab.bean.OrdersDetail;
+import com.yanzi.bean.OdCustom;
+import com.yanzi.bean.OrdersDetail;
 
 public interface IOrdersDetail {
 	
