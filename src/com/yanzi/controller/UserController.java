b37ec7f981ba406963704b91ccf48736763e023c
dxr2587698334 <2587698334@qq.com>
2019-05-05 18:53:59 +0800

    Create UserController.java

---
+++ src/com/yanzi/controller/UserController.java
@@ -0,0 +1,91 @@
+package com.yanzi.controller;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.openlab.bean.Privilege;
+import com.openlab.bean.User;
+import com.openlab.service.IPrivilege;
+import com.openlab.service.IUser;
+import com.openlab.util.VerifyCode;
+
+//标识这歌类是Controller
+@Controller
+// 请求该UserController的路径
+@RequestMapping("UserController")
+public class UserController {
+
+	@Autowired
+	IUser iUser;
+
+	@Autowired
+	IPrivilege iPrivilege;
+	
+	//生成验证码
+	@RequestMapping("/getVerifyCode")
+	public void getVerifyCode(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		/*
+		 * 1. 生成图片
+		 * 2. 保存图片上的文本到session域中
+		 * 3. 把图片响应给客户端
+		 */
+		VerifyCode vc = new VerifyCode();
+		BufferedImage image = vc.getImage();
+		//保存图片上的文本到session域
+		request.getSession().setAttribute("session_vcode", vc.getText());
+		VerifyCode.output(image, response.getOutputStream());
+	}
+
+	// 登录
+	@RequestMapping("/login")
+	public String login(HttpSession session, String uid, String upwd,String nimgs) {
+		//获取验证码
+		String sessionVcode = (String) session.getAttribute("session_vcode");
+		User user = iUser.login(uid, upwd);
+		List<Privilege> privilegeUserList = iPrivilege.findUserPrivilege(uid);
+		if(nimgs.equals(sessionVcode)){
+			if (user != null) {
+				session.setAttribute("user", user);
+				session.setAttribute("privilegeUserList", privilegeUserList);
+				return "redirect:/main.jsp";
+			} else {
+				return "redirect:/Login.jsp";
+			}
+		}else{
+			return "redirect:/Login.jsp";
+		}
+	}
+
+	// 注册
+	@RequestMapping("/addUser")
+	public String adUser(User user) {
+		iUser.addUser(user);
+		return "redirect:/Login.jsp";
+	}
+
+	@RequestMapping("/Exit")
+	public String Exit(HttpSession session) {
+		session.invalidate();
+		return "redirect:/Login.jsp";
+	}
+
+	// 修改个人信息
+	@RequestMapping("/updateUserInfo")
+	public String updateUserInfo(HttpSession session, User user) {
+		iUser.updateUserInfo(user);
+		User user2 = iUser.findByIdUser2(user.getUid());
+		session.removeAttribute("user");
+		session.setAttribute("user", user2);
+		return "redirect:/InfoUser.jsp";
+	}
+
+}
